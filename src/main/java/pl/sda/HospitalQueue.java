41995de4a9d285bffcdbaa7f80b2a72ecc131f62
cezklosowski <cez.klosowski@gmail.com>
2020-06-15 22:44:04 +0200

    1. done

---
+++ src/main/java/pl/sda/HospitalQueue.java
@@ -0,0 +1,8 @@
+package pl.sda;
+
+import java.util.Queue;
+
+public class HospitalQueue {
+    private Queue patientQueue;
+
+}
---
+++ src/main/java/pl/sda/Patient.java
@@ -0,0 +1,50 @@
+package pl.sda;
+
+public class Patient {
+    private String name;
+    private String surname;
+    private int howMuchAngry;
+    private String diagnosedDisease;
+
+    public Patient() {
+    }
+
+    public Patient(String name, String surname, int howMuchAngry, String diagnosedDisease) {
+        this.name = name;
+        this.surname = surname;
+        this.howMuchAngry = howMuchAngry;
+        this.diagnosedDisease = diagnosedDisease;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getHowMuchAngry() {
+        return howMuchAngry;
+    }
+
+    public void setHowMuchAngry(int howMuchAngry) {
+        this.howMuchAngry = howMuchAngry;
+    }
+
+    public String getDiagnosedDisease() {
+        return diagnosedDisease;
+    }
+
+    public void setDiagnosedDisease(String diagnosedDisease) {
+        this.diagnosedDisease = diagnosedDisease;
+    }
+}
